584bd97cad76855e5e905037bc0d52ace758c69a
Emran <emranhstbq@gmail.com>
2020-09-03 11:59:37 +0600

    Navigating Programmatically

--- src/containers/Blog/Posts/Posts.js
+++ src/containers/Blog/Posts/Posts.js
@@ -31,7 +31,8 @@ class Posts extends Component {
     }
 
     passidFullpost = (id) =>{
-        this.setState({fullPostId: id});
+        // this.props.history.push('/'+id)
+        this.props.history.push({pathname: '/'+id})
     }
 
     render() {
@@ -43,14 +44,14 @@ class Posts extends Component {
         if(!this.state.error){
             posts = this.state.posts.map((post, i) => {
                 return (
-                    <Link to={'/'+post.id} key={post.id}>
+                    // <Link to={'/'+post.id} key={post.id}>
                         <Post 
-                            
+                            key={post.id}
                             title={post.title}
                             author={post.author}
                             clicked={() => this.passidFullpost(post.id)}
                         />
-                    </Link>
+                    // </Link>
                 )
             })
         }
